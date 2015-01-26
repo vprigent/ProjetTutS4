6cea38c8ba9bdb4a73bd1d125859cd51d66adf48
maximedasilva <maximedasilva40@gmail.com>
2015-01-26 14:58:36 +0100

    modification du main

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world first");
+		System.out.println("Hello world Second");
 	}
 
 }
