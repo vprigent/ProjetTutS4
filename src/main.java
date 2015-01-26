8e9a0942327709a63777b2d165ac2f918f6fb2fd
Vincent Prigent <vincent.prigent@hotmail.fr>
2015-01-26 14:26:47 +0100

    Premier commit de famille, changement du gitignore

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world first");
+	}
+
+}
