02c14ea81dd65d8ea8ad025a6a3bcd3e9a7fc34e
MatLacoste <mathieu33_lacoste@hotmail.fr>
2015-02-19 20:51:48 +0100

    Implémentation de l'algorithme Random

--- src/model/algorithm/RandomLayout.java
+++ src/model/algorithm/RandomLayout.java
@@ -1,12 +1,48 @@
 package model.algorithm;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
 import model.Graph;
+import model.Node;
+import model.Shape;
 
 public class RandomLayout extends Algorithm {
 
     @Override
     public void algorithm(Graph g) {
-        // TODO Auto-generated method stub
+        ArrayList<Node> myNodes = g.getNodes();
+        
+        for (Node n : myNodes) {
+        	int r = 1 + (int)(Math.random()*(255-1)+1);
+        	int v = 1 + (int)(Math.random()*(255-1)+1);
+        	int b = 1 + (int)(Math.random()*(255-1)+1);
+        	n.setColor(new Color(r,v,b));
+        	
+        	int x = 1 + (int)(Math.random()*(800-1)+1);
+        	int y = 1 + (int)(Math.random()*(600-1)+1);
+            n.setPosition(x, y);
+            
+            int sh = 1 + (int)(Math.random()*(600-1)+1);
+            switch (sh) {
+            case 1 :
+            	n.setShape(Shape.SQUARE);
+            	break;
+            case 2 :
+            	n.setShape(Shape.CIRCLE);
+            	break;
+            case 3 :
+            	n.setShape(Shape.ROUND);
+            	break;
+            case 4 :
+            	n.setShape(Shape.TRIANGLE);
+            	break;
+            }
+            
+            int size = 1 + (int)(Math.random()*(4-1)+1);
+            n.setSize(size);
+        }
 
     }
 }
