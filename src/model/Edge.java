68ccbea3b3bd1bd18bd209821d5fdfcd6d6f4aba
Vincent Prigent <vincent.prigent@etu.u-bordeaux1.fr>
2015-03-07 19:02:32 +0100

    Modification size -> weight correspondant plus

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -7,9 +7,9 @@ import java.awt.*;
  */
 
 public class Edge {
-    public final int maxSize = 10;
+    public final int maxWeight = 10;
 
-    private int size;
+    private int weight;
     private String label;
     private Color color;
 
@@ -25,7 +25,7 @@ public class Edge {
      * @param destination Define the destination node
      */
     public Edge(Node source, Node destination) {
-        this.size = 1;
+        this.weight = 1;
         this.label = "";
         this.color = Color.black;
         this.source = source;
@@ -41,28 +41,28 @@ public class Edge {
      * @param isOriented if the edge is oriented, ie source and destination are importants
      * @param color the color of the edge
      * @param label label of the edge, informations attached
-     * @param size size of the edge
+     * @param weight weight of the edge
      */
-    public Edge(Node source, Node destination, boolean isOriented, Color color, String label, int size) {
+    public Edge(Node source, Node destination, boolean isOriented, Color color, String label, int weight) {
         this.source = source;
         this.destination = destination;
         this.isOriented = isOriented;
         this.color = color;
         this.label = label;
-        if(size > maxSize)
-            size = maxSize;
-        this.size = size;
+        if(weight > maxWeight)
+            weight = maxWeight;
+        this.weight = weight;
     }
 
-    public int getSize() {
-        return size;
+    public int getweight() {
+        return weight;
     }
 
     /**
-     * @param size the size to set
+     * @param weight the weight to set
      */
-    public void setSize(int size) {
-        if(this.size <= maxSize) this.size = size;
+    public void setweight(int weight) {
+        if(this.weight <= maxWeight) this.weight = weight;
     }
 
     public String getLabel() {
@@ -132,7 +132,7 @@ public class Edge {
     @Override
     public String toString() {
         return "Edge{" +
-                "size=" + size +
+                "weight=" + weight +
                 ", label='" + label + '\'' +
                 ", color=" + color +
                 ", source=" + source +
