505b76b6e98a0fb0273bc86cd46b394f24d86eba
Bastien Bournas <bastien.bournas@etu.u-bordeaux.fr>
2015-03-16 11:50:58 +0100

    Debut de la creation du JDialog Dialogproperties

---
+++ src/view/DialogProperties.java
@@ -0,0 +1,36 @@
+package view;
+import model.Node;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+/**
+ * Created by basbournas on 16/03/15.
+ */
+
+
+public class DialogProperties extends JDialog {
+
+    Node selected ;
+
+    public DialogProperties(JFrame parent, String title, boolean modal, Node selected){
+
+        //On appelle le construteur de JDialog correspondant
+        super(parent, title, modal);
+
+        //On spécifie une taille
+        this.setSize(200, 80);
+
+        //La position
+        this.setLocationRelativeTo(null);
+
+        //La boîte ne devra pas être redimensionnable
+        this.setResizable(false);
+
+        //Enfin on l'affiche
+        this.setVisible(true);
+
+
+        this.selected=selected;
+    }
+
+}
