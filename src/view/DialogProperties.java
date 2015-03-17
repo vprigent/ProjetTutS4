ff7807d7ecf3c5c158e5bbfb654b77bbdf42a769
maximedasilva <maximedasilva40@gmail.com>
2015-03-17 19:50:45 +0100

    debut du JDialog

--- src/view/DialogProperties.java
+++ src/view/DialogProperties.java
@@ -1,8 +1,10 @@
 package view;
+
 import model.Node;
 
-import javax.swing.JDialog;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by basbournas on 16/03/15.
  */
@@ -11,24 +13,36 @@ import javax.swing.JFrame;
 public class DialogProperties extends JDialog {
 
     Node selected ;
+    private JPanel tools;
+    private JButton test;
 
     public DialogProperties(JFrame parent, String title, boolean modal, Node selected){
 
         //On appelle le construteur de JDialog correspondant
-        super(parent, title, modal);
+       super(parent, title, modal);
 
         //On spécifie une taille
-        this.setSize(200, 80);
+        this.setSize(5000, 370);
+        JPanel messagePane = new JPanel();
+        messagePane.add(new JLabel(title));
+        getContentPane().add(messagePane);
+        JPanel buttonPane = new JPanel();
+        JButton button = new JButton("OK");
+        buttonPane.add(button);
+       // button.addActionListener(this);
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
+        pack();
+        setVisible(true);
+
 
         //La position
         this.setLocationRelativeTo(null);
 
         //La boîte ne devra pas être redimensionnable
-        this.setResizable(false);
+        this.setResizable(true);
 
         //Enfin on l'affiche
         this.setVisible(true);
-
 
         this.selected=selected;
     }
