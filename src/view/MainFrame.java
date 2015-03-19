d5892244fbcf89e383bd0250420f160786c70aff
Vincent Prigent <vincent.prigent@etu.u-bordeaux1.fr>
2015-03-19 22:34:58 +0100

    lookandfeel potable

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -8,6 +9,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
 
 import model.GraphHandler;
 import controller.Controller;
@@ -19,14 +21,15 @@ public class MainFrame extends JFrame implements Observer {
     private GraphHandler model;
 
     private JComboBox<String> algorithms;
-
     private JComboBox<String> displayMode;
+
     private DrawingPanel mainPanel;
 
     public MainFrame(Controller controller) {
         this.controller = controller;
-		this.setVisible(true);
-		initComponents();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        initComponents();
 	}
 	
 	// <editor-fold defaultstate="collapsed" desc="Generated Code">
@@ -45,197 +48,207 @@ public class MainFrame extends JFrame implements Observer {
         }
 
 		mainPanel = new DrawingPanel(controller.addDrawingController());
-        JMenuBar toolBar = new JMenuBar();
         JMenuBar menuBar = new JMenuBar();
+        JToolBar toolBar = new JToolBar();
 
         JMenu fileMenu = new JMenu("Fichiers");
-        JMenuItem newButton = new JMenuItem();
-        JMenuItem loadButton = new JMenuItem();
-        JMenuItem saveButton = new JMenuItem();
+        JMenuItem newButton = new JMenuItem("Nouveau");
+        JMenuItem loadButton = new JMenuItem("Charger");
+        JMenuItem saveButton = new JMenuItem("Sauvegarder");
 
         JMenu modifier = new JMenu("Modifier");
-        JMenuItem removeButton = new JMenuItem();
+        JMenuItem remove = new JMenuItem("Tout supprimer");
         JMenuItem undo = new JMenuItem("Annuler");
         JMenuItem redo = new JMenuItem("Répéter");
+
+        JMenu about = new JMenu("A propos");
+        JMenuItem help = new JMenuItem("Aide");
 
 		displayMode = new JComboBox<String>();
 		algorithms = new JComboBox<String>();
 
-        JMenuItem zoomIn = new JMenuItem();
-        JMenuItem zoomOut = new JMenuItem();
-        JMenuItem copy = new JMenuItem();
-        JMenuItem cut = new JMenuItem();
-        JMenuItem paste = new JMenuItem();
-        JMenuItem add = new JMenuItem();
-        JMenuItem delete = new JMenuItem();
+        JButton zoomIn = new JButton();
+        JButton zoomOut = new JButton();
+        JButton copy = new JButton();
+        JButton cut = new JButton();
+        JButton paste = new JButton();
+        JButton add = new JButton();
+        JButton delete = new JButton();
+        JButton undoButton = new JButton();
+        JButton redoButton = new JButton();
 
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setTitle("Graph Drawer");
-		setMinimumSize(new java.awt.Dimension(800, 400));
-		setName("MainFrame"); // NOI18N
-		setPreferredSize(new java.awt.Dimension(800, 600));
+		setMinimumSize(new Dimension(800, 400));
+		setName("MainFrame");
+		setPreferredSize(new Dimension(800, 600));
 
+        // add elements on menu bar
         fileMenu.add(newButton);
         fileMenu.add(loadButton);
         fileMenu.add(saveButton);
         menuBar.add(fileMenu);
-        this.setJMenuBar(menuBar);
 
-        modifier.add(removeButton);
+        modifier.add(remove);
         modifier.add(undo);
         modifier.add(redo);
         menuBar.add(modifier);
+        this.setJMenuBar(menuBar);
 
-        this.setLocationRelativeTo(null);
+        toolBar.add(zoomIn);
+        toolBar.add(zoomOut);
+        toolBar.add(displayMode);
+        toolBar.add(algorithms);
+        toolBar.add(copy);
+        toolBar.add(cut);
+        toolBar.add(paste);
+        toolBar.add(add);
+        toolBar.add(delete);
+        toolBar.add(undoButton);
+        toolBar.add(redoButton);
+        toolBar.setFloatable(false);
+        this.add(toolBar, BorderLayout.PAGE_START);
+        this.add(mainPanel, BorderLayout.CENTER);
 
-        toolBar.setVisible(true);
-        this.setContentPane(toolBar);
 
-		newButton.setBackground(new java.awt.Color(65, 65, 65));
-		newButton.setFocusable(false);
-		newButton.setText("Nouveau");
-		newButton.setName("new"); // NOI18N
 		newButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 newButtonActionPerformed(evt);
             }
         });
 
-		loadButton.setBackground(new java.awt.Color(65, 65, 65));
-		loadButton.setFocusable(false);
-		loadButton.setText("Charger");
-		loadButton.setName("load"); // NOI18N
 		loadButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 loadButtonActionPerformed(evt);
             }
         });
 
-		saveButton.setBackground(new java.awt.Color(65, 65, 65));
-		saveButton.setFocusable(false);
-		saveButton.setText("Sauvegarder");
-		saveButton.setName("save"); // NOI18N
 		saveButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 saveButtonActionPerformed(evt);
             }
         });
 
-		removeButton.setBackground(new java.awt.Color(65, 65, 65));
-		removeButton.setFocusable(false);
-		removeButton.setText("Supprimer");
-		removeButton.setName("remove"); // NOI18N
-		removeButton.addActionListener(new ActionListener() {
+		remove.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 removeButtonActionPerformed(evt);
             }
         });
 
-		GroupLayout menuBarLayout = new GroupLayout(menuBar);
-
-		undo.setIcon(new ImageIcon(getClass().getResource(
-                "icons/DataContainer_MovePreviousHS.png"))); // NOI18N
-		undo.setFocusPainted(false);
-		undo.setFocusable(false);
-		undo.setName("undo"); // NOI18N
+        undo.setIcon(new ImageIcon(getClass().getResource(
+                "icons/DataContainer_MovePreviousHS.png")));
 		undo.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 undoActionPerformed(evt);
             }
         });
 
-		redo.setIcon(new ImageIcon(getClass().getResource(
-                "icons/DataContainer_MoveNextHS.png"))); // NOI18N
-		redo.setFocusPainted(false);
-		redo.setFocusable(false);
-		redo.setName("undo"); // NOI18N
+        undoButton.setIcon(undo.getIcon());
+        undoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                undoActionPerformed(evt);
+            }
+        });
+
+        redo.setIcon(new ImageIcon(getClass().getResource(
+                "icons/DataContainer_MoveNextHS.png")));
 		redo.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 redoActionPerformed(evt);
             }
         });
 
-		displayMode.setModel(new DefaultComboBoxModel<String>(new String[] {
-				"Tout afficher", "Sommets", "Arrêtes" }));
-		displayMode.setFocusable(false);
+        redoButton.setIcon(redo.getIcon());
+        redo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                redoActionPerformed(evt);
+            }
+        });
+
+        displayMode.setModel(new DefaultComboBoxModel<String>(new String[]{
+                "Tout afficher", "Sommets", "Arrêtes"}));
 		displayMode.addItemListener(new ItemListener() {
+            @Override
 			public void itemStateChanged(ItemEvent evt) {
 				displayModeItemStateChanged(evt);
-			}
-		});
+            }
+        });
 
-		algorithms.setModel(new DefaultComboBoxModel<String>(new String[] {
-				"Aucun Algorithme", "Aleatoire", "Circulaire",
-				"Modele de force", "Colorisation", "Calcul d'indice", "Taille",
-				"Personnalisé" }));
-		algorithms.setFocusable(false);
+        algorithms.setModel(new DefaultComboBoxModel<String>(new String[]{
+                "Aucun Algorithme", "Aleatoire", "Circulaire",
+                "Modele de force", "Colorisation", "Calcul d'indice", "Taille",
+                "Personnalisé"}));
 		algorithms.addItemListener(new ItemListener() {
+            @Override
 			public void itemStateChanged(ItemEvent evt) {
 				algorithmsItemStateChanged(evt);
-			}
-		});
+            }
+        });
 
-		zoomIn.setIcon(new ImageIcon(getClass().getResource("icons/zoomIn.png"))); // NOI18N
-		zoomIn.setFocusable(false);
+        zoomIn.setIcon(new ImageIcon(getClass().getResource("icons/zoomIn.png")));
 		zoomIn.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 zoomInActionPerformed(evt);
             }
         });
 
-		zoomOut.setIcon(new ImageIcon(getClass().getResource(
-                "icons/zoomOut.png"))); // NOI18N
-		zoomOut.setFocusable(false);
+        zoomOut.setIcon(new ImageIcon(getClass().getResource("icons/zoomOut.png")));
 		zoomOut.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 zoomOutActionPerformed(evt);
             }
         });
 
-		copy.setIcon(new ImageIcon(getClass().getResource("icons/copy.png"))); // NOI18N
-		copy.setFocusable(false);
+        copy.setIcon(new ImageIcon(getClass().getResource("icons/copy.png")));
 		copy.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 copyActionPerformed(evt);
             }
         });
 
-		cut.setIcon(new ImageIcon(getClass().getResource("icons/cut.png"))); // NOI18N
-		cut.setFocusable(false);
+        cut.setIcon(new ImageIcon(getClass().getResource("icons/cut.png")));
 		cut.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 cutActionPerformed(evt);
             }
         });
 
-		paste.setIcon(new ImageIcon(getClass().getResource("icons/paste.png"))); // NOI18N
-		paste.setFocusable(false);
+        paste.setIcon(new ImageIcon(getClass().getResource("icons/paste.png")));
 		paste.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 pasteActionPerformed(evt);
             }
         });
 
-		add.setIcon(new ImageIcon(getClass().getResource("icons/add.png"))); // NOI18N
+        add.setIcon(new ImageIcon(getClass().getResource("icons/add.png"))); //
 		add.setToolTipText("");
-		add.setFocusable(false);
 		add.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 addActionPerformed(evt);
             }
         });
 
-		delete.setIcon(new ImageIcon(getClass().getResource("icons/delete.png"))); // NOI18N
-		delete.setFocusable(false);
+        delete.setIcon(new ImageIcon(getClass().getResource("icons/delete.png")));
 		delete.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent evt) {
                 deleteActionPerformed(evt);
             }
         });
-
-		GroupLayout mainPanelLayout = new GroupLayout(mainPanel);
-		mainPanel.setLayout(mainPanelLayout);
-        mainPanel.setLocation(0, 70);
 
 		pack();
 	}// </editor-fold>
